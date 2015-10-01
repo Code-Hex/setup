7aefe1913af3230ac738571d314a4f01ea9d7b1b
K <Code-Hex@users.noreply.github.com>
2015-10-01 12:27:08 +0900

    added cpanm
    added cpanm

--- setup-4.sh
+++ setup-4.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/sh
 
 set -eux
+
+cpanm -nq git://github.com/miyagawa/cpanminus.git@menlo
+cpanm -nq git://github.com/shoichikaji/cpm.git
+
+PLMLIST=(LWP::UserAgent Mojolicious Carton Caroline Reply Term::ReadLine::EditLine)
+
+for item in ${PLMLIST[@]}; do
+	cpm install --global $item
+done
+
 CASKLIST=(0xed 1password appcleaner aquaterm coteditor cyberduck dropbox font-source-code-pro google-chrome skype sublime-text3 torbrowser virtualbox vlc wireshark xquartz)
 
 for item in ${CASKLIST[@]}; do
