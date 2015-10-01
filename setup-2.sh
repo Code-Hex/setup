3d543f83e4b2a7f39daaf6cb1d4a33f0f0bbbed8
K <Code-Hex@users.noreply.github.com>
2015-10-01 12:16:14 +0900

    fixed
    git clone git@ to git clone https://

--- setup-2.sh
+++ setup-2.sh
@@ -16,10 +16,10 @@ brew tap homebrew/science
 brew install automake cmake cowsay dpkg ffmpeg fish fortune ghostscript git highlight imagemagick
 brew install libtool lua nkf openssl readline ricty sl sqlite toilet tree wget zsh zsh-completions
 
-git clone git@github.com:Code-Hex/zshrc.git
+git clone https://github.com/Code-Hex/zshrc.git
 cp zshrc/.zshrc zshrc/coin.wav zshrc/jump.wav zshrc/exit.wav ./
 
-git clone git@github.com:Code-Hex/vimrc.git
+git clone https://github.com/Code-Hex/vimrc.git
 cp vimrc/.vimrc ./
 
 git clone https://github.com/tomasr/molokai ./.vim/
